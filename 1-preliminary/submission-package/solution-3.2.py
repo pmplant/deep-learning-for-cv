45315d1000883270f6bd4d09e8d2aacce743f8c5
toaplant <paul.molina.plant@gmail.com>
2019-01-12 16:03:15 -0800

    fixed 3.3

--- 1-preliminary/submission-package/solution-3.2.py
+++ 1-preliminary/submission-package/solution-3.2.py
@@ -3,14 +3,19 @@ from PIL import Image
 import numpy as np
 import h5py as h5
 
+
+def show_arr(arr):
+    Image.fromarray(arr).show()
+
+
 if __name__ == "__main__":
     # load image
     img = Image.open('input.jpg')
     img.show()
     # invert image
-    inv = Image.fromarray(255 - np.asarray(img))
-    inv.show()
+    inv = 255 - np.asarray(img)
+    show_arr(inv)
     # save inverted image
     with h5.File('output.h5', 'w') as f:
-        f.create_dataset('inv', data=inv)
+        f.create_dataset('arr', data=inv)
         f.close()
--- 1-preliminary/submission-package/solution-3.3.py
+++ 1-preliminary/submission-package/solution-3.3.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
+from sys import argv
+from PIL import Image
+from scipy import ndimage
+import numpy as np
+import h5py as h5
+
+
+def show_arr(arr):
+    Image.fromarray(arr).show()
+
 
 if __name__ == "__main__":
-    # Main code goes here
-
-    exit(0)
+    # params
+    k1 = int(argv[1])
+    k2 = int(argv[2])
+    # load h5
+    f = h5.File('output.h5', 'r')
+    arr = f['arr'][:]
+    # to single channel
+    gs = np.ndarray((len(arr), len(arr[0])), dtype=np.uint8)
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            val = np.array(int(np.mean(arr[i][j])))
+            gs[i][j] = np.uint8(np.mean(arr[i][j]))
+    show_arr(gs)
+    # apply filters
+    g1 = ndimage.gaussian_filter(gs, sigma=k1)
+    show_arr(g1)
+    g2 = ndimage.gaussian_filter(g1, sigma=k2)
+    show_arr(g2)
