faa46663e140cdec4c007f4e4ac0c295876d1075
toaplant <paul.molina.plant@gmail.com>
2019-01-13 10:24:36 -0800

    done coding

--- 1-preliminary/submission-package/solution-3.3.py
+++ 1-preliminary/submission-package/solution-3.3.py
@@ -4,7 +4,6 @@ from scipy.ndimage.filters import gaussian_filter
 import numpy as np
 import h5py as h5
 from matplotlib import pyplot as plt
-import matplotlib.cm as cm
 
 if __name__ == "__main__":
     # sigma params
