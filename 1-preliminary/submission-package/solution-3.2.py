53de9d76b0dc4aa4450e92bca0b4b1868414766c
toaplant <paul.molina.plant@gmail.com>
2019-01-12 17:24:33 -0800

    finally finished 3.2 and 3.3

--- 1-preliminary/submission-package/solution-3.2.py
+++ 1-preliminary/submission-package/solution-3.2.py
@@ -2,20 +2,22 @@
 from PIL import Image
 import numpy as np
 import h5py as h5
-
-
-def show_arr(arr):
-    Image.fromarray(arr).show()
-
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     # load image
     img = Image.open('input.jpg')
-    img.show()
+
     # invert image
     inv = 255 - np.asarray(img)
-    show_arr(inv)
+
     # save inverted image
     with h5.File('output.h5', 'w') as f:
         f.create_dataset('arr', data=inv)
         f.close()
+
+    # show images
+    plt.imshow(img)
+    plt.show()
+    plt.imshow(inv)
+    plt.show()
--- 1-preliminary/submission-package/solution-3.3.py
+++ 1-preliminary/submission-package/solution-3.3.py
@@ -4,30 +4,29 @@ from PIL import Image
 from scipy.ndimage.filters import gaussian_filter
 import numpy as np
 import h5py as h5
-
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 
-def show_arr(arr):
-    Image.fromarray(arr).show()
-
-
 if __name__ == "__main__":
-    # params
+    # sigma params
     k1 = int(argv[1])
     k2 = int(argv[2])
+
     # load h5
     f = h5.File('output.h5', 'r')
     arr = f['arr'][:]
-    show_arr(arr)
-    # to single channel
+
+    # reduce to a single channel
     gs = arr.mean(axis=2)
-    # apply filters
+
+    # apply DoG filter
     g1 = gaussian_filter(gs, sigma=k1, mode='reflect')
     g2 = gaussian_filter(gs, sigma=k2, mode='reflect')
     dif = g2 - g1
-    show_arr(dif)
-    norm = dif/255.0
-    print(norm[0][0])
-    show_arr(norm)
+
+    # normalize image
+    norm = (dif - np.amin(dif)) / np.amax(dif)
+
+    # show image
     plt.imshow(norm, cmap=cm.Greys_r)
+    plt.show()
